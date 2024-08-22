18097c22cc700d0d5e5ec69b27797f60eeb7979f
joao-nzuzi <joaonzuzi21@gmail.com>
2024-08-22 15:45:15 +0100

    Initial commit

---
+++ src/main/java/nzuzi/joao/criacao_consulta_numero_bi/CriacaoConsultaNumeroBiApplication.java
@@ -0,0 +1,13 @@
+package nzuzi.joao.criacao_consulta_numero_bi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriacaoConsultaNumeroBiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriacaoConsultaNumeroBiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nzuzi/joao/criacao_consulta_numero_bi/CriacaoConsultaNumeroBiApplicationTests.java
@@ -0,0 +1,13 @@
+package nzuzi.joao.criacao_consulta_numero_bi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriacaoConsultaNumeroBiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

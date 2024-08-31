cf1bc6ff7cdac260ef5ce82e656861e3c40cf704
joao-nzuzi <joaonzuzi21@gmail.com>
2024-08-31 20:50:15 +0100

    feat: criação do caso de teste de sucesso que verifica se o cidadao tem idade para tratar bilhete de identidade ou não

--- src/test/java/nzuzi/joao/criacao_consulta_numero_bi/model/IdentificacaoCidadaoTest.java
+++ src/test/java/nzuzi/joao/criacao_consulta_numero_bi/model/IdentificacaoCidadaoTest.java
@@ -1,4 +1,16 @@
+package nzuzi.joao.criacao_consulta_numero_bi.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 class IdentificacaoCidadaoTest {
-  
+
+    @Test
+    void temIdadeParaTerBilheteDeIdentidade() {
+        IdentificacaoCidadao cidadao = new IdentificacaoCidadao("Joao", LocalDate.of(2018, 1, 1));
+        Assertions.assertEquals("test", "test");
+    }
 }
